b9ac38daeeaff0a6eb9ad40dc32c21a597008454
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-16 14:42:35 -0400

    add MagicFruit test

---
+++ FruitBurstUnitTests/MagicFruitTest.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FruitBurstBackend;
+using System;
+
+namespace FruitBurstUnitTests
+{
+    [TestClass]
+    public class MagicFruitTest
+    {
+
+        [TestMethod]
+        public void TestMagicFruitIsVisible()
+        {
+            IFruit mgFruit = new MagicFruit(new Fruit());
+            Assert.IsFalse(mgFruit.IsVisible);
+        }
+
+        [TestMethod]
+        public void TestMagicFruitHP()
+        {     
+            MagicFruit magicF = new MagicFruit(new Fruit());
+            Assert.AreEqual(20, magicF.HP);
+        }
+
+        [TestMethod]
+        public void TestMagicFruitMakeInvisible()
+        {
+            MagicFruit mgFruit = new MagicFruit(new Fruit());
+            mgFruit.IsVisible = true;
+            mgFruit.MakeInvisible();
+
+            Assert.AreEqual(true, mgFruit.IsVisible);
+        }
+
+        [TestMethod]
+        public void TestMagicFruitMakeVisible()
+        {
+            IFruit mgFruit = new MagicFruit(new Fruit());
+            mgFruit.MakeVisible();
+            Assert.IsTrue(mgFruit.IsVisible);
+
+        }
+
+        
+    }
+}
