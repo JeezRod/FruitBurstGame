f4b3a64223a0959d846d501f77e1ea7bf7c25c79
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-15 20:30:04 -0400

    add IFruit Interface

---
+++ FruitBurstBackend/IFruit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FruitBurstBackend
+{
+    public interface IFruit
+    {
+        int Points{get;}
+        bool IsVisible{get;set;}
+
+        void MakeVisible();
+        void MakeInvisible();
+    }
+}
