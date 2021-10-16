6a9215e517a38cc73eb4e2c969e48916395bfc69
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-16 14:40:17 -0400

    IsVisible variable fix

--- FruitBurstBackend/Fruit.cs
+++ FruitBurstBackend/Fruit.cs
@@ -5,12 +5,16 @@ namespace FruitBurstBackend
     
     public class Fruit: IFruit{
         private Random rand = new Random();
+
+        public Fruit(){
+            IsVisible = false;
+        }
         
         public int Points{get {return rand.Next(1,6);}}
 
         public bool IsVisible{
-                get {return false;}
-                set{IsVisible = value;}
+                get;
+                set;
             }
 
         public void MakeVisible(){
