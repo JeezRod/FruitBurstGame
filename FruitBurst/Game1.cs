1290671a8b4ef3d924b9467df317e4905e19de76
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-17 22:50:00 -0400

    update update method

--- FruitBurst/Game1.cs
+++ FruitBurst/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using FruitBurstBackend;
+using System;
 
 namespace FruitBurst
 {
@@ -10,15 +11,23 @@ namespace FruitBurst
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private GameState gameState;
-        private ScoreAndLevel scoreAndLevel;
         private FruitGridSprite fruitGridSprite;
         private ScoreSprite scoreSprite;
+        private MouseState previous;
+        private MouseState current;
+        private const int maxScore = 300;
 
+        private const int rectan = 100;
+        private int counter = 0;
+        private int timer = 0;
+        private int interval =120;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            current = Mouse.GetState();
+            previous = current;
         }
 
         protected override void Initialize()
@@ -28,9 +37,8 @@ namespace FruitBurst
             _graphics.PreferredBackBufferWidth = 1000;
             _graphics.ApplyChanges();
             gameState = new GameState(8,8);
-            scoreAndLevel = new ScoreAndLevel();
             fruitGridSprite = new FruitGridSprite(this, gameState.Grid);
-            scoreSprite = new ScoreSprite(this, scoreAndLevel);
+            scoreSprite = new ScoreSprite(this, gameState.ScoreAndLevelCounter);
             Components.Add(fruitGridSprite);
             Components.Add(scoreSprite);
             base.Initialize();
@@ -45,11 +53,35 @@ namespace FruitBurst
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
+            
             // TODO: Add your update logic here
+            if(counter == interval){
+                gameState.MakeFruitsAppear();
+                counter = 0;
+            }
+            counter++;
+            timer++;
 
+            if(timer == 1000){
+                interval /= 2;
+                timer = 0;
+                gameState.IncrementLevel();
+            }
+
+
+            previous = current;
+            current = Mouse.GetState();
+            if(current.LeftButton == ButtonState.Pressed && previous.LeftButton == ButtonState.Released){
+                for(int i = 0; i < gameState.Grid.Height; i++){
+                    for(int j = 0; j < gameState.Grid.Width; j++){
+                        Rectangle r = new Rectangle(i*rectan,j*rectan, rectan, rectan);
+                        if(r.Contains(current.Position.X, current.Position.Y) && gameState.Grid.GetVisibilityAt(i,j) ){
+                            gameState.Grid[i,j].MakeInvisible();
+                            gameState.UpdateScore(gameState.Grid[i,j]);
+                        }
+                    }
+                }
+            }
             base.Update(gameTime);
         }
 
